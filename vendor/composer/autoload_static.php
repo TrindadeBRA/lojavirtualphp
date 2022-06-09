5b0543abe2d4609f313185795d540051fb25e2f1
Lucas Trindade <trindadebra@gmail.com>
2022-06-09 00:38:42 -0300

    preparacao do ambiente

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitead85f7cf621953ba87975685adcf745
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/trinityweb/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInitead85f7cf621953ba87975685adcf745
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitead85f7cf621953ba87975685adcf745::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitead85f7cf621953ba87975685adcf745::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitead85f7cf621953ba87975685adcf745::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitead85f7cf621953ba87975685adcf745::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitead85f7cf621953ba87975685adcf745::$classMap;
 
---
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -0,0 +1,69 @@
+<?php 
+
+namespace Hcode\DB;
+
+class Sql {
+
+	const HOSTNAME = "69.49.241.26";
+	const USERNAME = "thetri13_localhost";
+	const PASSWORD = "SMiCo^Tv&5bT";
+	const DBNAME = "thetri13_db-ecommerce";
+
+	private $conn;
+
+	public function __construct()
+	{
+
+		$this->conn = new \PDO(
+			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME, 
+			Sql::USERNAME,
+			Sql::PASSWORD
+		);
+
+	}
+
+	private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
